c3dd7c84791e534f91db8eb846fa441ce0f96982
Liisa Mikola <liismik@users.noreply.github.com>
2020-09-15 10:02:16 +0300

    Kolmanda tunni esimene pool tehtud

---
+++ Tund 3/home_1st_week.php
@@ -0,0 +1,36 @@
+<?php
+   $username = "Liisa Mikola";
+   $fulltimenow = date("d.m.Y H:i:s");
+   $hournow = date("H");
+   $partofday = "lihtsalt aeg";
+   if($hournow < 6){
+			$partofday = "uneaeg";
+   } //enne 6 
+   if($hournow >= 8 and $hournow <= 18){
+			$partofday = "õppimise aeg";
+   }
+   
+   //vaatame semestri kulgemist
+   $semesterstart = new DateTime("2020-8-31");
+   $semesterend = new DateTime("2020-12-13");
+   $semesterduration = $semesterstart->diff($semesterend);
+   $semesterdurationdays = $semesterduration->format("%r%a");
+   $today = new DateTime("now");
+   
+   
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">  
+  <title><?php echo $username; ?> programmeerib veebi</title>
+  
+</head>
+<body>
+  <h1><?php echo $username; ?></h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate Instituudis.</p>
+  <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+  <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+</body>
+</html>
